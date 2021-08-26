1511d999b3ec2eef6f22e5ec07b90fee9dd06e6c
Jonathan Henrique Granado Perroni <66213929+jhowzitos@users.noreply.github.com>
2021-08-26 01:08:17 -0300

    finalizando contato

--- Contato.php
+++ Contato.php
@@ -1,31 +1,31 @@
 
 
 <h2>C O N T A T O S</h2>
-<div class="row">
-<section>
-    <article>
-        <div class="row">
-            <form action="https://api.staticforms.xyz/submit" method="POST" classe="card">
+<div class="card">
+
+        
+            <form  class="unicesumar" > 
             <input type="hidden" name="accessKey" value="b31a59d2-c1d0-406c-a7a0-4e777b92115b">
             <input type="hidden" name="redirectTo" value="http://localhost/back-end-i/index.php?pagina=Contato.php">
 
                 <label for="inputNome">Nome:</label>
                 <input type="text" id="inputNome" name="name" required>
-                <span></span>
+                <span></span> <br>
 
                 <label for="inputEmail">Email:</label>
                 <input type="Email" id="inputEmail" name="email" required>
-                <span></span>
+                <span></span><br>
 
                 <label for="textareaMensagem">Mensagem:</label>
               <textarea name="message" id="textareaMensagem" required></textarea>
                 <span></span>
 
               <button>Enviar Mensagem</button>
+              <p>
+                  
+              </p>
             </form>
-        </div>
+        
    
-    </article>
-
-</section>
+   
 </div>
